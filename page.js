5c15bc3159dca25fa56ccea9924d6ee53ccf8797
Zeito <josejoaozeito@gmail.com>
2024-02-24 08:59:48 -0800

    First section finished

--- page.js
+++ page.js
@@ -5,3 +5,8 @@ const body = document.querySelector("body")
 themeToggler.addEventListener("click", () => {
     body.classList.toggle("dark")
 })
+
+/*            animation-name: ponto, largura;
+            animation-duration: .5s, 5s;
+            animation-timing-function: ease;
+            animation-iteration-count: 1;*/
