5b15b172b02cacc7afcd5e13d7dac27756faea26
Zeito <josejoaozeito@gmail.com>
2024-02-25 13:05:41 -0800

    POr hj e tudo

--- page.js
+++ page.js
@@ -1,12 +1,15 @@
-const themeToggler = document.querySelector(".color-mode") // Butao do light/dark mode 
-const body = document.querySelector("body")
-
+const themeToggler = document.querySelector(".color-mode"); // Butao do light/dark mode
+const body = document.querySelector("body");
 
 themeToggler.addEventListener("click", () => {
-    body.classList.toggle("dark")
-})
+    body.classList.toggle("dark");
+});
 
-/*            animation-name: ponto, largura;
-            animation-duration: .5s, 5s;
-            animation-timing-function: ease;
-            animation-iteration-count: 1;*/
+//O codigo a seguir pertence a pagina "About" e tem a ver com a interatividade quando se passa o mouse sobre as fotos da "galeria"
+
+const gallery = document.querySelector(".gallery");
+const wrapper = document.querySelector(".wrapper");
+
+gallery.addEventListener("mousemove", (e) => {
+    wrapper.style.left = `-${(e.x - 377) * 1.6}px`;
+});
